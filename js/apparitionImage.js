5e5bfc40fe50a16f3a35aa9ca4c2673337989245
Raphael <raphaelarzoin173@gmail.com>
2025-02-24 22:14:55 +0100

    image assouline

---
+++ js/apparitionImage.js
@@ -0,0 +1,15 @@
+// Sélectionner toutes les images avec la classe 'fade-in-image'
+const images = document.querySelectorAll('.fade-in-image');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target); // Arrête d'observer l'image après qu'elle soit apparue
+    }
+  });
+}, { threshold: 0.2 }); // Seuil de visibilité de 50%
+
+images.forEach(image => {
+  observer.observe(image); // Observer chaque image
+});
