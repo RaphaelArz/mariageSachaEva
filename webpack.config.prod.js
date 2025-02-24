bd65dac7b43eda172092ea462876e57fa4103fc1
Raphael <raphaelarzoin173@gmail.com>
2025-02-24 20:39:09 +0100

    set up nouveau projet

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './indexParallax.html',
     }),
     new CopyPlugin({
       patterns: [
