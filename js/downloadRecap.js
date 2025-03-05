5467a980a6df446bb3819e8ee61f02765d3dc3d9
Raphael <raphaelarzoin173@gmail.com>
2025-03-05 20:02:49 +0100

    add recap

--- js/downloadRecap.js
+++ js/downloadRecap.js
@@ -1,11 +1,25 @@
 function downloadImage() {
-  const imageUrl = "img/recapSachaEva.png"; // Chemin vers ton image
+  // Récupérer l'URL actuelle
+  const pathname = window.location.pathname;
+
+  // Associer des images spécifiques à chaque page
+  let imageUrl = "/img/recapSachaEva.png"; // Image par défaut
+
+  if (pathname.includes("index.html")) {
+    imageUrl = "/img/recapSachaEva1.png";
+  } else if (pathname.includes("mhs.html")) {
+    imageUrl = "/img/recapSachaEva2.png";
+  } else if (pathname.includes("mhsc.html")) {
+    imageUrl = "/img/recapSachaEva3.png";
+  }
+
+  // Téléchargement de l'image sélectionnée
   fetch(imageUrl)
     .then(response => response.blob())
     .then(blob => {
       const link = document.createElement("a");
       link.href = URL.createObjectURL(blob);
-      link.download = "recapSachaEva.png"; // Nom du fichier
+      link.download = imageUrl.split("/").pop(); // Garde le nom du fichier
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
